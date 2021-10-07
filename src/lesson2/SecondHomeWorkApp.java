ebc0f9fd511eec0cffd178cca1a65e97d333ef36
Maria Kuzmina <bigcat82@gmail.com>
2021-10-07 19:43:55 +0300

    Lesson 2 homework

--- src/lesson2/SecondHomeWorkApp.java
+++ src/lesson2/SecondHomeWorkApp.java
@@ -12,61 +12,40 @@ public class SecondHomeWorkApp {
         System.out.println("Задание 4:");
         printStrNTimes("Test", 5);
         System.out.println("Задание 5:");
-        printIfLeapYear(2020);
-
+        printIfLeapYear(2100);
     }
 
-    public static boolean checkSum(int a, int b)
-    {
-        if (a + b >= 10 && a + b <= 20)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    public static boolean checkSum(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
     }
 
-    public static void printIfPositiveNegative(int value)
-    {
-        if (value >= 0)
-        {
-            System.out.println("Число " + value + " положительное" );
+    public static void printIfPositiveNegative(int value) {
+        if (value >= 0) {
+            System.out.println("Число " + value + " положительное");
         }
-        else
-        {
-            System.out.println("Число " + value + " отрицательное" );
+        else {
+            System.out.println("Число " + value + " отрицательное");
         }
     }
 
 
-    public static boolean ifPositiveNegative(int value)
-    {
-        if (value >= 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    public static boolean ifPositiveNegative(int value) {
+        return value >= 0;
     }
 
-    public static void printStrNTimes(String str, int n)
-    {
-        for (var i=0; i < n; i++)
-        {
+    public static void printStrNTimes(String str, int n) {
+        for (var i=0; i < n; i++) {
             System.out.println(str);
         }
     }
 
-    public static void printIfLeapYear(int year)
-    {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    public static void printIfLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Год " + year + " високосный");
-        else
+        }
+        else {
             System.out.println("Год " + year + " не високосный");
+        }
 
     }
 }
