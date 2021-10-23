ace346c99a04f432ac00e2c0e45ebf19fb1ddc3c
Maria Kuzmina <bigcat82@gmail.com>
2021-10-23 20:44:06 +0300

    lesson 6 homework

---
+++ src/lesson6/Cat.java
@@ -0,0 +1,12 @@
+package lesson6;
+
+public class Cat extends Animal {
+
+    public Cat(String name,  int runLimit) {
+        super(name, 0, runLimit);
+    }
+
+
+}
+
+
---
+++ src/lesson6/Dog.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+public class Dog extends Animal{
+    public Dog(String name, int swimLimit, int runLimit) {
+        super(name, swimLimit, runLimit);
+    }
+}
